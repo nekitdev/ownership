94b32a5db7c22e6f1a6180f552596ca189e4e203
Nikita Tikhonov <nekitdevofficial@gmail.com>
2025-08-28 10:49:48 +0300

    Move to separate `reverse_into_owned` function.

--- src/cmp.rs
+++ src/cmp.rs
@@ -2,11 +2,15 @@ use core::cmp::{Ordering, Reverse};
 
 use crate::IntoOwned;
 
+fn reverse_into_owned<T: IntoOwned>(reverse: Reverse<T>) -> Reverse<T::Owned> {
+    Reverse(reverse.0.into_owned())
+}
+
 impl<T: IntoOwned> IntoOwned for Reverse<T> {
     type Owned = Reverse<T::Owned>;
 
     fn into_owned(self) -> Self::Owned {
-        Reverse(self.0.into_owned())
+        reverse_into_owned(self)
     }
 }
 
